60416b3ddcbe95f2e10e9cadb57089c94f7439b0
正能由佳 <yuka.shono0228@gmail.com>
2015-11-14 13:18:29 +0900

    change var

--- GitApplication/ViewController.swift
+++ GitApplication/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var number :Int = 0
+    var number :Int = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
