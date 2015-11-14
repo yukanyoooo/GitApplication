a1b0c8d44b2064dec4f9cb9192695a0010e26b11
正能由佳 <yuka.shono0228@gmail.com>
2015-11-14 13:15:43 +0900

    initial commit

---
+++ GitApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitApplication
+//
+//  Created by 正能由佳 on 2015/11/14.
+//  Copyright (c) 2015年 Yuka Shono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
